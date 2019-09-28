9cb1a3d6359f2054e129f18a974a648fb6cbb9c3
Sharaaf Nazeer <ahamedsharaaf@gmail.com>
2019-09-28 19:15:36 +0530

    new updates with validations

---
+++ GigHub/Controllers/GigController.cs
@@ -0,0 +1,56 @@
+﻿using GigHub.Models;
+using GigHub.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+
+namespace GigHub.Controllers
+{
+    public class GigController : Controller
+    {
+        private ApplicationDbContext _context;
+        public GigController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Gig
+        [HttpGet]
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Genres = _context.Genres.ToList();
+                return View(viewModel);
+            }
+
+            var gig = new Gig
+            {
+                ArtistId = User.Identity.GetUserId(),
+                Venue = viewModel.Venue,
+                GenreId = viewModel.Genre,
+                DateTime = viewModel.GetDateTime()
+            };
+
+            _context.Gigs.Add(gig);
+            _context.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ GigHub/Migrations/201909281102072_PopulateGenreTable.cs
@@ -0,0 +1,20 @@
+namespace GigHub.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateGenreTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Genres(Id, Name) VALUES(1, 'Jazz')");
+            Sql("INSERT INTO Genres(Id, Name) VALUES(2, 'Blues')");
+            Sql("INSERT INTO Genres(Id, Name) VALUES(3, 'Rock')");
+        }
+        
+        public override void Down()
+        {
+            Sql("DELETE FROM Genres WHERE IN(1,2,3)");
+        }
+    }
+}
