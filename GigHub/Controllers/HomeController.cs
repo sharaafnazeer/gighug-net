4aabbc7c0fcc339b309cd58ba9b82d30c0483121
Sharaaf Nazeer <ahamedsharaaf@gmail.com>
2019-09-28 22:03:17 +0530

    Improved home page

--- GigHub/Controllers/HomeController.cs
+++ GigHub/Controllers/HomeController.cs
@@ -18,7 +18,9 @@ namespace GigHub.Controllers
 
         public ActionResult Index()
         {
-            var upcomingGigs = _context.Gigs.Include(g => g.Artist)
+            var upcomingGigs = _context.Gigs
+                .Include(g => g.Artist)
+                .Include(g => g.Genre)
                 .Where(g => g.DateTime> DateTime.Now);
             return View(upcomingGigs);
         }
